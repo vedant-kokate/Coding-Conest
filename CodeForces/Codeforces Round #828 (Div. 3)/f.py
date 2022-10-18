7e22f3f89e8cdca55331d84d377f3a1f83518a5a
vedantkokate07 <vedant.kokate07@gmail.com>
2022-10-18 11:26:16 +0530

    codeforces round 828

--- CodeForces/Codeforces Round #828 (Div. 3)/f.py
+++ CodeForces/Codeforces Round #828 (Div. 3)/f.py
@@ -6,28 +6,26 @@ def I():return list(map(int,input().split()))
 from math import gcd,sqrt
 
 def f(a,b,c,d):
-    n=a
-    fact={}
-    for i in range(2,int(sqrt(a))+2):
-        while n%i==0:
-            n//=i
-            if i in fact:
-                fact[i]+=1
-            else:
-                fact[i]=1
-    if n>1:
-        fact[n]=1
-    print(fact)
-    # val=a*b
-    # for i in range(a+1,c+1):
-    #     n2=val//gcd(val, i)
-    #     # print(n2,"n2",i,val)
-    #     if n2<=d:
-    #         n3=(d//n2)*n2
-    #         # print(n3,b,d)
-    #         if b<n3<=d:
-    #             return i,n3
-    # return -1,-1
+   
+
+    val=a*b
+    for i in range(a+1,int(sqrt(c+1))+1):
+        n2=val//gcd(val, i)
+        # print(n2,"n2",i,val)
+        if n2<=d:
+            n3=(d//n2)*n2
+            # print(n3,b,d)
+            if b<n3<=d and a<i<=c:
+                return i,n3
+        if val%i==0 and a
+        n2=val//gcd(val, i)
+        # print(n2,"n2",i,val)
+        if n2<=d:
+            n3=(d//n2)*n2
+            # print(n3,b,d)
+            if b<n3<=d:
+                return i,n3
+    return -1,-1
 for _ in range(int(input())):
     a,b,c,d=I()
     print(*f(a,b,c,d))
